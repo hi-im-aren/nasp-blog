7e896d525ccb89b625d7806e47f3ecf85ad0dff6
Balazs Berta <balazs.berta.dev@gmail.com>
2023-11-28 13:29:22 +0100

    v0.1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,16 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      backgroundImage: {
+        headerBackground: "url('/src/assets/multicolor_glows.png')",
+      },
       fontFamily: {
         sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwindcss-animated"),
+  ],
 };
