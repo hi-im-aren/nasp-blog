44e867a7dd29359ea126ef9cc31f7b218409845c
Balazs Berta <balazs.berta.dev@gmail.com>
2023-11-28 13:40:37 +0100

    Build error fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import preact from '@astrojs/preact'
-import react from '@astrojs/react'
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hi-im-aren.github.io",
-  base: "nasp-blog",
+  site: "https://cisco-open.github.io",
+  base: "nasp",
   integrations: [
     tailwind(),
     image({
